e6d6dea01b846ecf916ecef2347bc3b0a38d218b
Matthew Taylor <mat@sharrow.me.uk>
2020-11-15 12:21:34 +0000

    adding example code from boost website - Compiliation fails! (I wouldn't normally check-in code that doesn't build but this is just to show working ;)

--- boost-example/TopologicalSort/main.cpp
+++ boost-example/TopologicalSort/main.cpp
@@ -4,6 +4,20 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
-    return 0;
+    typedef adjacency_list< vecS, vecS, directedS, color_property<> > Graph;
+    typedef boost::graph_traits<Graph>::vertex_descriptor Vertex;
+    Pair edges[6] = { Pair(0,1), Pair(2,4),
+                      Pair(2,5),
+                      Pair(0,3), Pair(1,4),
+                      Pair(4,3) };
+    Graph G(6, edges, edges + 6);
+
+    typedef std::vector< Vertex > container;
+    container c;
+    topological_sort(G, std::back_inserter(c));
+
+    cout << "A topological ordering: ";
+    for ( container::reverse_iterator ii=c.rbegin(); ii!=c.rend(); ++ii)
+      cout << index(*ii) << " ";
+    cout << endl;
 }
