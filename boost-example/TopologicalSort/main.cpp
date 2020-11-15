6c76624421f94692296477d338c1bb3a82a0c4fc
Matthew Taylor <mat@sharrow.me.uk>
2020-11-15 12:23:55 +0000

    added includes and using directive - better, but still not compiling

--- boost-example/TopologicalSort/main.cpp
+++ boost-example/TopologicalSort/main.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
+#include <boost/graph/graph_traits.hpp>
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/topological_sort.hpp>
 
 using namespace std;
+using namespace boost;
 
 int main()
 {
-    typedef adjacency_list< vecS, vecS, directedS, color_property<> > Graph;
+    typedef adjacency_list<vecS, vecS, directedS, color_property<>> Graph;
     typedef boost::graph_traits<Graph>::vertex_descriptor Vertex;
     Pair edges[6] = { Pair(0,1), Pair(2,4),
                       Pair(2,5),
@@ -12,7 +16,7 @@ int main()
                       Pair(4,3) };
     Graph G(6, edges, edges + 6);
 
-    typedef std::vector< Vertex > container;
+    typedef std::vector<Vertex> container;
     container c;
     topological_sort(G, std::back_inserter(c));
 
