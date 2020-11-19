8b28335e6d776d2ed909ba2911462d6d147c77c0
Matthew Taylor <mat@sharrow.me.uk>
2020-11-19 15:53:04 +0000

    updated cli so that it ran all the examples and printed input and out to make it easier to tell what's happening

--- tsortcli/main.cpp
+++ tsortcli/main.cpp
@@ -9,7 +9,24 @@ using namespace std;
 
 int main()
 {
-    auto input = R"INPUT(a =>
+    std::string inputs[6];
+    inputs[0] = "a =>";
+    inputs[1] = R"INPUT(a =>
+b =>
+c =>)INPUT";
+    inputs[2] = R"INPUT(a =>
+b => c
+c =>)INPUT";
+    inputs[3] = R"INPUT(a =>
+b => c
+c => f
+d => a
+e => b
+f =>)INPUT";
+    inputs[4] = R"INPUT(a =>
+b =>
+c => c)INPUT";
+    inputs[5] = R"INPUT(a =>
 b => c
 c => f
 d => a
@@ -17,12 +34,19 @@ e =>
 f => b)INPUT";
 
     OTBLibrary otbl;
-    try {
-        auto joblist = otbl.sort_jobs(input);
-        std::cout << joblist << std::endl;
-    }
-    catch (std::invalid_argument & e) {
-        std::cerr << "Sorting Jobs failed. Because: " << e.what() << std::endl;
+    for(auto & input : inputs) {
+
+        try {
+            std::cout << "About to sort the input: " << std::endl << input << std::endl;
+
+            auto joblist = otbl.sort_jobs(input);
+
+            std::cout << std::endl << "Output:" << std::endl;
+            std::cout << joblist << std::endl << std::endl;
+        }
+        catch (std::invalid_argument & e) {
+            std::cerr << std::endl << "Sorting input jobs failed. Because: " << e.what() << std::endl << std::endl;
+        }
     }
 
     return 0;
